c1826c871993256b2ced5030ae1fb53a27bddbc4
Roni Saha <roni.cse@gmail.com>
2013-01-25 11:00:30 +0600

    First Version Release

--- README.md
+++ README.md
@@ -2,3 +2,72 @@ viafoura-follow-widget
 ======================
 
 Easy Custom Follow Link Integration of Viafoura
+
+Key Features
+============
+* Easy to integrate with any Application working with Viafoura
+* Support Follow status!
+* Next to zero configuration!!
+
+
+Current Active Version
+======================
+v 0.1 Released!
+
+
+Installation
+------------
+
+### Prequisites
+
+ * [jQuery](http://www.jquery.com) - This is required by Viafoura Service it self.
+
+
+### Required files
+
+Copy `ViafouraFollowWidget.js` to your javascript folder.
+
+Usage
+-----
+
+If you like demos more than a boring documentation see the `example.html` file and play with it.
+
+The plugin can be called with jQuery in different ways.
+
+### Standard call with default theme and settings:
+
+    $('.myElement').ViafouraFollowWidget();
+
+#### Html content and markup
+
+any Html container tags(`<div>`,`<span>`, `<td>`) etc. can be used. replace {USERID} with appropriate viafoura user ID
+
+    <div class="myElement" data-id="{USERID}"></div>
+
+You can also provide the text in javascript for all link elements:
+
+    $('.myElement').ViafouraFollowWidget({
+                                           followLabelText:"Follow Link",
+                                           unFollowLabelText:"Unfollow Link"
+                                        });
+
+You can change the default value for all options.
+
+
+### Options
+
+ * followLabelText: Text To show as Follow Link.
+ * unFollowLabelText: Text To show as Un follow link.
+
+Feedback
+--------
+
+Please send me an [email](mailto:roni@xiidea.net) or a tweet ‏@XiiDEA with any feedback you have.
+
+This plugin is my first attempt at a custom widget for Viafoura Integrated System, so any ideas for improvement are welcome.
+
+
+Contributing
+------------
+
+Contribute!
---
+++ ViafouraFollowWidget.js
@@ -0,0 +1,144 @@
+
+(function( $ ){
+    var createCookie = function (name, value) {
+        var expires = "";
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    var DEFAULT_SETTINGS = {
+        followLabelText:"Follow",
+        unFollowLabelText:"Unfollow"
+    };
+
+    var allWidgets = [];
+    var methods = {
+        init : function( options ) {
+            var settings = $.extend({}, DEFAULT_SETTINGS, options || {});
+
+            return this.each(function(){
+                var el = $(this);
+                var widgetObject = new LoginWidget(el, settings);
+                if(typeof Viafoura == 'undefined'){
+                    allWidgets.push(widgetObject);
+                }else{
+                    widgetObject.AddListeners();
+                }
+                $(this).data("widgetObject", widgetObject);
+            });
+
+        }
+    };
+
+    $.fn.ViafouraFollowWidget = function( method ) {
+        if ( methods[method] ) {
+            return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+        } else if ( typeof method === 'object' || ! method ) {
+            return methods.init.apply( this, arguments );
+        } else {
+            $.error( 'Method ' + method + ' does not exist on jQuery.ViafouraFollowWidget' );
+        }
+
+    };
+
+    var LoginWidget = function (el, settings) {
+        var self = this;
+        self.followQueue = false;
+        self.userId = el.attr('data-id');
+
+        this.bindEvents = function(){
+
+            el.click(function(e){
+                e.preventDefault();
+                if(el.hasClass('followed')){
+                    Viafoura.publish('/services/unfollow',self.userId);
+                    self.updateFollowStatus(false);
+                }else{
+                    Viafoura.publish('/services/follow',self.userId);
+                    if(!(self.Viafoura.current.user.id && self.Viafoura.current.user.id != 0)){
+                        self.followQueue = true;
+                    }else{
+                        self.updateFollowStatus(true);
+                    }
+                }
+            });
+        };
+
+        this.updateFollowStatus = function(status){
+            if(status){
+                el.addClass('followed');
+                el.html(settings.unFollowLabelText);
+            }else{
+                el.removeClass('followed');
+                el.html(settings.followLabelText);
+            }
+        }
+
+        this.getUser = function(userId, callback){
+            var data = {verb:"get", session:window.sessionStorage._vf_sid};
+            self.Ajax("/users/"+userId, {},callback);
+        };
+
+        this.Ajax = function(route,data,callback){
+            var url = self.siteSettings.api_url+"?callback=?";
+            var jsonParam = '{"requests":{"data":{"verb":"get","route":"'+route+'"}},' +
+                '"site":"'+self.siteSettings.domain+'","session":"'+window.sessionStorage._vf_sid+'"}';
+
+            var data = {json: jsonParam};
+            jQuery.getJSON(url, {json: jsonParam}, function (data,status,xhr) {
+                var returnValue = data.responses.data || false;
+                callback.call(self,returnValue.result,status,xhr)
+            })
+        };
+
+        this.AddListeners = function(){
+            self.Viafoura = Viafoura.core;
+            self.bindEvents();
+            self.siteSettings = Viafoura.core.locations;
+
+            Viafoura.subscribe("/response/follows/" + self.userId,function(e, t, n, r){
+                self.updateFollowStatus(r.data.verb !== "delete");
+            });
+
+
+            Viafoura.subscribe('/user/login/success',function(){
+
+                if(self.Viafoura.current.user.id && self.Viafoura.current.user.id != 0){ //Logout
+                    createCookie('vf_session', window.sessionStorage._vf_sid);
+
+                    self.getUser(self.userId,function(result,status,xhr){
+                        self.updateFollowStatus(result.subscribed);
+                        if(self.followQueue && !result.subscribed){
+                            Viafoura.publish('/services/follow',self.userId);
+                        }
+                        self.followQueue = false;
+                    });
+                }else{
+                    self.updateFollowStatus(false);
+                    createCookie('vf_session', "");
+                }
+            });
+        };
+    };
+
+
+
+    var ViafouraDependentInit = function (callableFunction){
+        if(typeof Viafoura == 'undefined'){
+            setTimeout(function(){
+                ViafouraDependentInit(callableFunction)
+            },10);
+            return;
+        }
+        callableFunction();
+    };
+
+    var ViafouraLoaded = function(){
+        if(allWidgets.length == 0){
+            return;
+        }
+        for (var index = 0; index < allWidgets.length; index++) {
+            allWidgets[index].AddListeners();
+        }
+    };
+    ViafouraDependentInit(ViafouraLoaded);
+})( jQuery );
